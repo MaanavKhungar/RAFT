215562ee0843724198426960e22544cdd70fa5c8
Maanav <khungarmaanav@gmail.com>
2024-06-07 13:14:27 +0200

    tensorfile

---
+++ tesnortry.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+import io
+import itertools
+from packaging import version
+
+import tensorflow as tf
+from tensorflow import keras
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn.metrics
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = \
+    fashion_mnist.load_data()
+
+# Names of the integer classes, i.e., 0 -> T-short/top, 1 -> Trouser, etc.
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+    'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+print("Shape: ", train_images[0].shape)
+print("Label: ", train_labels[0], "->", class_names[train_labels[0]]) 
+
+# Reshape the image for the Summary API.
+img = np.reshape(train_images[0], (-1, 28, 28, 1))
+
+print("TensorFlow version: ", tf.__version__)
+assert version.parse(tf.__version__).release[0] >= 2, \
+    "This notebook requires TensorFlow 2.0 or above."
+
+logdir = "logs/train_data/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+# Creates a file writer for the log directory.
+file_writer = tf.summary.create_file_writer(logdir)
+
+# Using the file writer, log the reshaped image.
+with file_writer.as_default():
+  tf.summary.image("Training data", img, step=0)
